d5035f89325cfde68783c6c69790b64439252149
prancingmad <prancingmad@gmail.com>
2025-09-09 21:22:44 -0500

    character_classes.py and config.py started

--- functions/character_classes.py
+++ functions/character_classes.py
@@ -1,87 +1,75 @@
+from config import *
+
 class BaseClass:
-    def __init__(self, level):
+    def __init__(self, name, level, mod):
+        self.name = name
         self.level = level
+        self.mod = mod
 
     def get_combat_value(self):
-        pass
-
-    def to_dict(self):
-        pass
+        return self.level * self.mod
 
 class Artificer(BaseClass):
     def __init__(self, level, primary):
-        super().__init__(level)
-        self.name = "Artificer"
+        super().__init__("Artificer", level, ARTI_MOD)
         self.primary = primary
 
 class Barbarian(BaseClass):
     def __init__(self, level, primary):
-        super().__init__(level)
-        self.name = "Barbarian"
+        super().__init__("Barbarian", level, BARB_MOD)
         self.primary = primary
 
 class Bard(BaseClass):
     def __init__(self, level, primary):
-        super().__init__(level)
-        self.name = "Bard"
+        super().__init__("Bard", level, BARD_MOD)
         self.primary = primary
 
 class Cleric(BaseClass):
     def __init__(self, level, primary):
-        super().__init__(level)
-        self.name = "Cleric"
+        super().__init__("Cleric", level, CLER_MOD)
         self.primary = primary
 
 class Druid(BaseClass):
     def __init__(self, level, primary):
-        super().__init__(level)
-        self.name = "Druid"
+        super().__init__("Druid", level, DRUI_MOD)
         self.primary = primary
 
 class Fighter(BaseClass):
     def __init__(self, level, primary):
-        super().__init__(level)
-        self.name = "Fighter"
+        super().__init__("Fighter", level, FIGH_MOD)
         self.primary = primary
 
 class Monk(BaseClass):
     def __init__(self, level, primary):
-        super().__init__(level)
-        self.name = "Monk"
+        super().__init__("Monk", level, MONK_MOD)
         self.primary = primary
 
 class Paladin(BaseClass):
     def  __init__(self, level, primary):
-        super().__init__(level)
-        self.name = "Paladin"
+        super().__init__("Paladin", level, PALA_MOD)
         self.primary = primary
 
 class Ranger(BaseClass):
     def __init__(self, level, primary):
-        super().__init__(level)
-        self.name = "Ranger"
+        super().__init__("Ranger", level, RANG_MOD)
         self.primary = primary
 
 class Rogue(BaseClass):
     def __init__(self, level, primary):
-        super().__init__(level)
-        self.name = "Rogue"
+        super().__init__("Rogue", level, ROGU_MOD)
         self.primary = primary
 
 class Sorcerer(BaseClass):
     def __init__(self, level, primary):
-        super().__init__(level)
-        self.name = "Sorcerer"
+        super().__init__("Sorcerer", level, SORC_MOD)
         self.primary = primary
 
 class Warlock(BaseClass):
     def __init__(self, level, primary):
-        super().__init__(level)
-        self.name = "Warlock"
+        super().__init__("Warlock", level, WARL_MOD)
         self.primary = primary
 
 class Wizard(BaseClass):
     def __init__(self, level, primary):
-        super().__init__(level)
-        self.name = "Wizard"
+        super().__init__("Wizard", level, WIZA_MOD)
         self.primary = primary
--- functions/player.py
+++ functions/player.py
@@ -2,3 +2,34 @@ class Player():
     def __init__(self, armor_class, magic_items):
         self.armor_class = armor_class
         self.magic_items = magic_items
+
+
+# placeholder
+
+combat_value = 0
+        arti_level = sum(cls.level for cls in self.classes if cls.name == "Artificer") * ARTI_MOD
+        barb_level = sum(cls.level for cls in self.classes if cls.name == "Barbarian") * BARB_MOD
+        bard_level = sum(cls.level for cls in self.classes if cls.name == "Bard") * BARD_MOD
+        cler_level = sum(cls.level for cls in self.classes if cls.name == "Cleric") * CLER_MOD
+        drui_level = sum(cls.level for cls in self.classes if cls.name == "Druid") * DRUI_MOD
+        figh_level = sum(cls.level for cls in self.classes if cls.name == "Fighter") * FIGH_MOD
+        monk_level = sum(cls.level for cls in self.classes if cls.name == "Monk") * MONK_MOD
+        pala_level = sum(cls.level for cls in self.classes if cls.name == "Paladin") * PALA_MOD
+        rang_level = sum(cls.level for cls in self.classes if cls.name == "Ranger") * RANG_MOD
+        rogu_level = sum(cls.level for cls in self.classes if cls.name == "Rogue") * ROGU_MOD
+        sorc_level = sum(cls.level for cls in self.classes if cls.name == "Sorcerer") * SORC_MOD
+        warl_level = sum(cls.level for cls in self.classes if cls.name == "Warlock") * WARL_MOD
+        wiza_level = sum(cls.level for cls in self.classes if cls.name == "Wizard") * WIZA_MOD
+        if barb_level >= 5 or figh_level >= 5 or monk_level >= 5 or pala_level >= 5 or rang_level >= 5:
+            combat_value += EXTRA_ATTACK_MOD
+        if barb_level >= 2:
+            combat_value += RECKLESS_MOD
+        if pala_level >= 2:
+            combat_value += SMITE_MOD
+        if figh_level >= 2:
+            combat_value += SURGE_MOD
+        if rogu_level >= 1:
+            combat_value += SNEAK_MOD
+        if monk_level >= 5:
+            combat_value += STUN_STRIKE_MOD
+        return combat_value + arti_level + barb_level + bard_level + cler_level + drui_level + figh_level + monk_level + pala_level + rang_level + sorc_level + warl_level + wiza_level
