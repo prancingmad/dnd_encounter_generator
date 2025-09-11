ceb832281ac114ab170a89411df999ad070d93ef
prancingmad <prancingmad@gmail.com>
2025-09-10 21:05:14 -0500

    Saving progress - placeholder_functions updated

--- functions/gui_functions.py
+++ functions/gui_functions.py
@@ -7,51 +7,6 @@ from .delete_party_member import *
 def placeholder_function(root):
     print("Button clicked - function not implemented yet")
 
-# Page Functions
-PAGE_FUNCTIONS = {
-    "Add Member": placeholder_function,
-    "Add Monster": placeholder_function,
-    "Archive": placeholder_function,
-    "Clear Data": placeholder_function,
-    "Close Program": placeholder_function,
-    "Delete Member": placeholder_function,
-    "Delete Monster": placeholder_function,
-    "Generate Encounter": placeholder_function,
-    "Main Page": placeholder_function,
-    "Manage Bestiary": placeholder_function,
-    "Manage Party": placeholder_function,
-    "Move Monster to Archive": placeholder_function,
-    "Move Monster to Random": placeholder_function,
-    "Move Monster to Required": placeholder_function,
-    "Random Encounters": placeholder_function,
-    "Required Encounters": placeholder_function,
-    "Update Member": placeholder_function
-}
-
-"""
-Each page function will need this to be updated as we make
-PAGE_FUNCTIONS = {
-    "Add Member": add_member,
-    "Add Monster": add_monster,
-    "Archive": archive_page,
-    "Clear Data": clear_data,
-    "Close Program": close_program,
-    "Delete Member": delete_member,
-    "Delete Monster": delete_monster,
-    "Generate Encounter": generate_encounter_page,
-    "Main Page": main_page,
-    "Manage Bestiary": manage_bestiary_page,
-    "Manage Party": manage_party_page,
-    "Move Monster to Archive": move_to_archive,
-    "Move Monster to Random": move_to_random,
-    "Move Monster to Required": move_to_required,
-    "Random Encounters": random_encounters_page,
-    "Required Encounters": required_encounters_page,
-    "Update Member": update_member
-}
-"""
-
-
 def on_button_click(label, root):
     func = PAGE_FUNCTIONS.get(label)
     if func:
@@ -82,3 +37,47 @@ def manage_party_page(root):
     for label in MANAGE_PARTY_BUTTON_LABELS:
         btn = tk.Button(button_frame, text=label, command=lambda l=label: on_button_click(l, root))
         btn.pack(**BUTTON_PACK_OPTIONS)
+
+# Page Functions
+PAGE_FUNCTIONS = {
+    "Add Member": placeholder_function,
+    "Add Monster": placeholder_function,
+    "Archive": placeholder_function,
+    "Clear Data": placeholder_function,
+    "Close Program": close_program,
+    "Delete Member": placeholder_function,
+    "Delete Monster": placeholder_function,
+    "Generate Encounter": placeholder_function,
+    "Main Page": main_page,
+    "Manage Bestiary": placeholder_function,
+    "Manage Party": manage_party_page,
+    "Move Monster to Archive": placeholder_function,
+    "Move Monster to Random": placeholder_function,
+    "Move Monster to Required": placeholder_function,
+    "Random Encounters": placeholder_function,
+    "Required Encounters": placeholder_function,
+    "Update Member": placeholder_function
+}
+
+"""
+Each page function will need this to be updated as we make
+PAGE_FUNCTIONS = {
+    "Add Member": add_member,
+    "Add Monster": add_monster,
+    "Archive": archive_page,
+    "Clear Data": clear_data,
+    "Close Program": close_program,
+    "Delete Member": delete_member,
+    "Delete Monster": delete_monster,
+    "Generate Encounter": generate_encounter_page,
+    "Main Page": main_page,
+    "Manage Bestiary": manage_bestiary_page,
+    "Manage Party": manage_party_page,
+    "Move Monster to Archive": move_to_archive,
+    "Move Monster to Random": move_to_random,
+    "Move Monster to Required": move_to_required,
+    "Random Encounters": random_encounters_page,
+    "Required Encounters": required_encounters_page,
+    "Update Member": update_member
+}
+"""
